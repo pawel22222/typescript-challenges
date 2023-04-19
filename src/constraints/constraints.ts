f1fea08a0d1da2e6bae2cf5f16e597bdfcb389f6
Pawel Sokolowski <pawelsokolowski22222@gmail.com>
2023-04-19 17:50:18 +0200

    solved: constraints

--- src/constraints/constraints.ts
+++ src/constraints/constraints.ts
@@ -36,7 +36,9 @@ const videos: Video[] = [
     { id: 5, name: 'Poznaj TypeScript', length: 22 }
 ]
 
-function processItems<T>(items: T[]): T[] {
+type processedItem = {id: number, name: string}
+
+function processItems<T extends processedItem>(items: T[]): processedItem[] {
     return items
         .filter(({ id }) => id > 2)
         .map(({ id, name }) => ({ id, name }));
